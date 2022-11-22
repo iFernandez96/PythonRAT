25e69a7ff42813a7c54da40e195976b1c07f871c
Israel <israelfernandez96@gmail.com>
2022-11-21 21:30:10 -0800

    A little bit of refactoring

--- PythonRAT.py
+++ PythonRAT.py
@@ -8,18 +8,13 @@ import subprocess
 def Exec(command):
     subprocess.Popen(command, shell=True)
 
-    # with subprocess.Popen(['/bin/sh', '-c'], 'pwd',stdout=subprocess.PIPE,stderr=subprocess.STDOUT) as proc: 
-    #     word = proc.stdout.read().strip()
-
-    # return word
-### Upload ###)
+### Upload ###
 def Upload(b64String, outputFile):
     
     OutputText = base64.b64encode(b64String)
     
     OutputFile = open(outputFile, 'wb')
     OutputFile.write(OutputText)
-    return
 
     
 ### Download ###
@@ -32,14 +27,11 @@ def Download(fileName):
 
 
 def main():
-    if __name__ == '__main__':
         print("Welcome to the Python RAT")
-        # b64String = "test"
-        # Upload(b64String, "Output.b64.txt")
 
         Download("test.txt")
         command = 'ls'
         Exec(command)
 
-
-main()
+if __name__ == '__main__':
+    main()
