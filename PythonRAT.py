6314380673a201ba98be7584110a5054dfc8a80c
Israel <israelfernandez96@gmail.com>
2022-11-21 18:30:24 -0800

    Hello World for Python RAT

---
+++ PythonRAT.py
@@ -0,0 +1,19 @@
+import sys
+import base64
+import socket
+
+### Execute ###
+def Exec():
+    pass
+### Upload ###
+def Upload():
+    pass
+### Download ###
+def Download():
+    pass
+
+
+
+if __name__ == '__main__':
+    
+    pass
